e339a7d794c406ed9cce734ea3f12c5ee31409f4
alvesjaov <victoralves169@outlook.com>
2024-11-08 21:53:42 -0300

    Atualizar configuração do Swagger para acesso na raiz e adicionar mensagem de inicialização da aplicação

--- minhaLoja/Program.cs
+++ minhaLoja/Program.cs
@@ -2,9 +2,11 @@ using Microsoft.EntityFrameworkCore;
 using minhaLoja.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+
 // Adicionando o DbContext aos serviços
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Outros serviços necessários para a aplicação
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
@@ -14,8 +16,16 @@ var app = builder.Build();
 
 // Habilitar o Swagger
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "minhaLoja API V1");
+    c.RoutePrefix = string.Empty; // Para acessar o Swagger na raiz (http://localhost:5140/)
+});
+
 app.UseAuthorization();
+
 app.MapControllers();
+
+Console.WriteLine("Aplicação iniciada. Acesse http://localhost:5140/swagger para ver a documentação da API.");
 
 await app.RunAsync();
